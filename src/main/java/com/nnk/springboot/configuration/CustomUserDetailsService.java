896f0e8e91f946092fef884479b246f8b2594f43
Sylakh <sylvainsds@gmail.com>
2024-05-26 16:19:56 +0200

    javadoc generation for security classes

--- src/main/java/com/nnk/springboot/configuration/CustomUserDetailsService.java
+++ src/main/java/com/nnk/springboot/configuration/CustomUserDetailsService.java
@@ -16,12 +16,24 @@ import org.springframework.stereotype.Service;
 import com.nnk.springboot.domain.DBUser;
 import com.nnk.springboot.repositories.UserRepository;
 
+/**
+ * Custom service for managing user details. Implements the
+ * {@link UserDetailsService} interface from Spring Security.
+ */
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepository;
 
+	/**
+	 * Loads a user by its username.
+	 *
+	 * @param username The username of the user.
+	 * @return A {@link UserDetails} object containing the user's details.
+	 * @throws UsernameNotFoundException if no user is found with the provided
+	 *                                   username.
+	 */
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Optional<DBUser> optionalUser = userRepository.findByUsername(username);
@@ -29,6 +41,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 		return new User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user.getRole()));
 	}
 
+	/**
+	 * Returns the list of authorities granted to a user based on their role.
+	 *
+	 * @param role The user's role.
+	 * @return A list of {@link GrantedAuthority}.
+	 */
 	private List<GrantedAuthority> getGrantedAuthorities(String role) {
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
--- src/main/java/com/nnk/springboot/configuration/SecurityConfig.java
+++ src/main/java/com/nnk/springboot/configuration/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Security configuration class for setting up Spring Security.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,31 +22,52 @@ public class SecurityConfig {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 
+	/**
+	 * Configures the security filter chain.
+	 *
+	 * @param http the {@link HttpSecurity} to modify.
+	 * @return the {@link SecurityFilterChain}.
+	 * @throws Exception if an error occurs when configuring the filter chain.
+	 */
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> {
-			auth.requestMatchers("/user/*").hasRole("ADMIN");
+			auth.requestMatchers("/user/**").hasRole("ADMIN");
+			auth.requestMatchers("/app/secure/article-details").hasRole("ADMIN"); // hidden door?
 
 			auth.anyRequest().authenticated();
 		}).sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
 				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).maximumSessions(1)
 				.expiredUrl("/login?session-expired=true").maxSessionsPreventsLogin(true))
-				.formLogin(form -> form.defaultSuccessUrl("/", true)).logout(logout -> logout.logoutUrl("/app-logout") // URL
-																														// de
-						// déconnexion
-						// par défaut
-						.logoutSuccessUrl("/login?logout") // Redirection après déconnexion
+				.formLogin(form -> form.defaultSuccessUrl("/", true))
+				.logout(logout -> logout.logoutUrl("/app-logout").logoutSuccessUrl("/login?logout") // Redirection après
+																									// déconnexion
 						.permitAll())
 				.exceptionHandling((exception) -> exception.accessDeniedPage("/app/error"));
 
 		return http.build();
 	}
 
+	/**
+	 * Creates a {@link PasswordEncoder} bean that uses BCrypt hashing algorithm.
+	 *
+	 * @return a {@link BCryptPasswordEncoder} instance.
+	 */
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	/**
+	 * Configures the {@link AuthenticationManager} with the custom user details
+	 * service and password encoder.
+	 *
+	 * @param http                  the {@link HttpSecurity} to configure.
+	 * @param bCryptPasswordEncoder the {@link BCryptPasswordEncoder} to use.
+	 * @return the configured {@link AuthenticationManager}.
+	 * @throws Exception if an error occurs when building the authentication
+	 *                   manager.
+	 */
 	@Bean
 	AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder)
 			throws Exception {
@@ -53,8 +77,5 @@ public class SecurityConfig {
 				.passwordEncoder(bCryptPasswordEncoder);
 		return authenticationManagerBuilder.build();
 	}
-	/**
-	 * @Bean HttpSessionEventPublisher httpSessionEventPublisher() { return new
-	 *       HttpSessionEventPublisher(); }
-	 */
+
 }
